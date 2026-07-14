20a2bccaa72d66c76a003707c3f341a4fe2526cd
handreicelsopetersen <handrei.celso@gmail.com>
2026-07-14 07:22:07 -0300

    ok

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+def my_view(request):
+    # Lógica da sua view aqui
+    return HttpResponse("Olá, esta é a minha view!")
+
+
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('', my_view, name='home'),
     path('admin/', admin.site.urls),
+    path('sobre/', my_view, name='sobre'),
+    path('sercompe/', RedirectView.as_view(url='https://sercompe.com.br'), name='sercompe'),
 ]
